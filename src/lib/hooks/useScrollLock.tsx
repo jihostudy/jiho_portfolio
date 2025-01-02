c7cae28db97b32d42f4e69c1a5e571620f0d51e0
JiHo00 <cale.jiho@gmail.com>
2025-01-02 21:46:38 +0900

    fix: 모달 닫을 시 Scroll smooth error
    change to instant screen shift

--- src/lib/hooks/useScrollLock.tsx
+++ src/lib/hooks/useScrollLock.tsx
@@ -3,18 +3,21 @@ import { useCallback } from 'react'
 const useScrollLock = () => {
   const lockScroll = useCallback(() => {
     document.body.style.cssText = `
-    position:fixed;
+    position: fixed;
     top: -${window.scrollY}px;
     overflow-y: auto;
     width: 100%;
     `
   }, [])
 
-  // 모달이 닫혔을 때 스크롤을 활성화 한다.
   const unLockScroll = useCallback(() => {
     const scrollY = document.body.style.top
+    document.documentElement.style.scrollBehavior = 'auto' // scroll-behavior를 일시적으로 비활성화
     document.body.style.cssText = ''
     window.scrollTo(0, parseInt(scrollY || '0', 10) * -1)
+    setTimeout(() => {
+      document.documentElement.style.scrollBehavior = '' // scroll-behavior를 원래 상태로 복구
+    }, 0)
   }, [])
 
   return { lockScroll, unLockScroll }
